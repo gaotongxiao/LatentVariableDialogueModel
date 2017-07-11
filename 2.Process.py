2c9e56170a8eee176ae5f61c8c0db79ea5f8a703
Gao Tong <gaotongxiao@gmail.com>
2017-07-11 12:20:27 +0800

    finished word embedding

--- 2.Process.py
+++ 2.Process.py
@@ -1,11 +1,15 @@
 import tensorflow as tf
 import collections
+import numpy as np
+import random
+import re
+import math
 
 '''
 Get word embedding vector
 '''
 f = open("data/segTrain.txt", "r")
-vocabulary = f.read().split()
+vocalbuary = f.read().split()
 f.seek(0)
 questions = []
 answers = []
@@ -21,7 +25,6 @@ vocabulary_size = 5000
 
 #get max length of one utterance to determine the max length of RNN
 rnn_length = len(max(questions + answers, key = len))
-print(rnn_length)
 
 def build_dataset(words, n_words):
   """Process raw inputs into a dataset."""
@@ -43,7 +46,99 @@ def build_dataset(words, n_words):
   reversed_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
   return data, count, dictionary, reversed_dictionary
 
-data, count, dictionary, reverse_dictionary = build_dataset(vocabulary,
+data, count, dictionary, reverse_dictionary = build_dataset(vocalbuary,
                                                             vocabulary_size)
 print('Most common words (+UNK)', count[:5])
 print('Sample data', data[:10], [reverse_dictionary[i] for i in data[:10]])
+
+data_index = 0
+
+# Step 3: Function to generate a training batch for the skip-gram model.
+def generate_batch(batch_size, num_skips, skip_window):
+  global data_index
+  assert batch_size % num_skips == 0
+  assert num_skips <= 2 * skip_window
+  batch = np.ndarray(shape=(batch_size), dtype=np.int32)
+  labels = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
+  span = 2 * skip_window + 1  # [ skip_window target skip_window ]
+  buffer = collections.deque(maxlen=span)
+  for _ in range(span):
+    buffer.append(data[data_index])
+    data_index = (data_index + 1) % len(data)
+  for i in range(batch_size // num_skips):
+    target = skip_window  # target label at the center of the buffer
+    targets_to_avoid = [skip_window]
+    for j in range(num_skips):
+      while target in targets_to_avoid:
+        target = random.randint(0, span - 1)
+      targets_to_avoid.append(target)
+      batch[i * num_skips + j] = buffer[skip_window]
+      labels[i * num_skips + j, 0] = buffer[target]
+    buffer.append(data[data_index])
+    data_index = (data_index + 1) % len(data)
+  # Backtrack a little bit to avoid skipping words in the end of a batch
+  data_index = (data_index + len(data) - span) % len(data)
+  return batch, labels
+
+'''
+batch, labels = generate_batch(batch_size=8, num_skips=2, skip_window=1)
+for i in range(8):
+  print(batch[i], reverse_dictionary[batch[i]],
+        '->', labels[i, 0], reverse_dictionary[labels[i,batch_size = 128]])
+        '''
+
+batch_size = 128
+embedding_size = 512  # Dimension of the embedding vector.
+skip_window = 1       # How many words to consider left and right.
+num_skips = 2         # How many times to reuse an input to generate a label. 0]])
+num_sampled = 64    # Number of negative examples to sample.
+
+train_inputs= tf.placeholder(tf.int32, shape=[batch_size])
+train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
+
+# Ops and variables pinned to the CPU because of missing GPU implementation
+with tf.device('/cpu:0'):
+# Look up embeddings for inputs.
+  embeddings = tf.Variable(
+        tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
+  embed= tf.nn.embedding_lookup(embeddings, train_inputs)
+
+  # Construct the variables for the NCE loss
+  nce_weights= tf.Variable(
+        tf.truncated_normal([vocabulary_size, embedding_size],
+                            stddev=1.0 / math.sqrt(embedding_size)))
+  nce_biases= tf.Variable(tf.zeros([vocabulary_size]))
+
+  # Compute the average NCE loss for the batch.
+  # tf.nce_loss automatically draws a new sample of the negative labels each
+  # time we evaluate the loss.
+  loss= tf.reduce_mean(
+        tf.nn.nce_loss(weights=nce_weights,
+                        biases=nce_biases,
+                        labels=train_labels,
+                        inputs=embed,
+                        num_sampled=num_sampled,
+                        num_classes=vocabulary_size))
+
+  # Construct the SGD optimizer using a learning rate of 1.0.
+  optimizer= tf.train.GradientDescentOptimizer(1.0).minimize(loss)
+
+with tf.Session() as sess:
+  sess.run(tf.global_variables_initializer())
+  average_loss = 0
+  for step in xrange(10001):
+    batch_inputs, batch_labels = generate_batch(
+        batch_size, num_skips, skip_window)
+    feed_dict = {train_inputs: batch_inputs, train_labels: batch_labels}
+
+    # We perform one update step by evaluating the optimizer op (including it
+    # in the list of returned values for session.run()
+    _, loss_val = sess.run([optimizer, loss], feed_dict=feed_dict)
+    average_loss += loss_val
+
+    if step % 2000 == 0:
+      if step > 0:
+        average_loss /= 2000
+      # The average loss is an estimate of the loss over the last 2000 batches.
+      print('Average loss at step ', step, ': ', average_loss)
+      average_loss = 0
